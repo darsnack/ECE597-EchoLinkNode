763bf7d35eabd0699bf31300b950e94172409588
Kyle Daruwalla <daruwalla.kyle@gmail.com>
2014-11-18 04:09:10 -0500

    Added status checking to start and stop node

--- website/client/app/main/main.controller.js
+++ website/client/app/main/main.controller.js
@@ -7,10 +7,6 @@ angular.module('w9naaEcholinkApp').controller('MainCtrl', function ($scope, $htt
     $scope.listMembers = function () {
     	console.log('angular');
 
-    	$http.get('/api/things/getNodeStatus').success(function (data) {
-    		$scope.nodeStatus = data.status ? 'Online' : 'Offline';
-    	});
-
     	$http.get('/api/things/listMembers').success(function (data) {
     		$scope.members = data.members;
     	});
@@ -18,10 +14,18 @@ angular.module('w9naaEcholinkApp').controller('MainCtrl', function ($scope, $htt
 
     $scope.startNode = function () {
     	$http.get('/api/things/startNode');
+
+    	$http.get('/api/things/getNodeStatus').success(function (data) {
+    		$scope.nodeStatus = data.status ? 'Online' : 'Offline';
+    	});
     };
 
     $scope.stopNode = function () {
     	$http.get('/api/things/stopNode');
+
+    	$http.get('/api/things/getNodeStatus').success(function (data) {
+    		$scope.nodeStatus = data.status ? 'Online' : 'Offline';
+    	});
     };
 
     $scope.getNodeStatus = function () {
