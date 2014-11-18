3232048bbc264bacb312e8155c88b9896fbe11d0
Kyle Daruwalla <daruwalla.kyle@gmail.com>
2014-11-17 22:54:31 -0500

    Added base website code

---
+++ website/client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('w9naaEcholinkApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
